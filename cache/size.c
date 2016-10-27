74e143e90ddf9cc7f8fca3d32f29b3732bb83f7e
grantal <AlexV348@gmail.com>
2016-10-26 20:01:51 -0700

    made some data

--- cache/size.c
+++ cache/size.c
@@ -10,35 +10,17 @@ Output should be in csv format but you'll have to use a '>' to capture it
 #include <stdlib.h>
 #include <time.h>
 #include <limits.h>
+#include "lib.h"
 
 // number of times we'll run the test for each arraysize
-const int ACCURACY = 10; 
+const int ACCURACY = 1; 
 // max memory to count up to (in bytes)
-const unsigned long MAX_MEMORY = 5000000;
-
-
-// gets an array size and an array then uses
-// that array by placing items in it and getting items out
-void reck_it(int arraysize, unsigned char* list){
-    // using a char array since a char takes up 1 byte
-    unsigned long i;
-    for (i = 0; i < arraysize; i++){
-        list[i] = (unsigned char)(i % UCHAR_MAX);
-    }
-    unsigned char y;
-    unsigned char k;
-    for (i = 0; i < arraysize; i++){
-        y = list[i]; 
-        k = y + 10;
-    }
-}
-
-
+const unsigned long MAX_MEMORY = 256000;
 
 int main(){
     
     unsigned long mem_size;
-    printf("function, input, cycles,\n");
+    printf("# memory size (bytes)\tcycles\n");
     for ( mem_size = 1000; mem_size < MAX_MEMORY; mem_size += 1000){ 
         // run test 'ACCURACY' times and then average the time
         int i;
@@ -55,7 +37,7 @@ int main(){
             timesum += diff;
             free(list);
         }
-        printf("reck_it, %lu, %ld,\n", mem_size, timesum/ACCURACY);
+        printf("%lu\t%ld\n", mem_size, timesum/ACCURACY);
     }
 }
 
